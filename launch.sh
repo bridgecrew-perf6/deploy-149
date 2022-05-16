0049eca99190bf87433adf489fe9c32b02c4cd7e
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2022-05-16 13:34:17 +0200

    Launcher changed to use mojitos

--- launch.sh
+++ launch.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+shopt -s expand_aliases
+source ~/.bash_aliases
 
-cd ./src
-make clean
-make all
-cd ../examples
-make clean
-make all
-./test_dot
+if ! command -v mojitos &> /dev/null
+then
+	echo "mojitos was not found, please install mojitos (https://gitlab.irit.fr/sepia-pub/mojitos) and use an alias, example:"
+    echo "echo \"alias mojitos='/home/$(whoami)/mojitos'\" >> /home/$(whoami)/.bash_aliases"
+    echo "Where /home/$(whoami)/mojitos is the directory wich contain the mojitos binary file"
+    echo "Don't forget to reload: source ~/.bash_aliases"
+    exit
+else	
+	mojitos -r -c -o $(pwd)/Results.csv -e $(pwd)/examples/test_dot    
+
+	
+fi
